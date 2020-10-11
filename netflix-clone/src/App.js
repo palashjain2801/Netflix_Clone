564187b77edcd19a538838fbccdff8bd66ee1ff6
palashjain2801 <palashjain2801@gmail.com>
2020-10-11 18:48:49 -0500

    update

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Row from './Row';
+import requests from "./requests";
 function App() {
   return (
     <div className="App">
-       <h1> Building Netflix clone</h1>
+      <h1> Building Netflix clone</h1>
+      <Row title="NETFLIX ORIGINAL" fetchUrl ={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl = {requests.fetchTrending}/>
     </div>
   );
 }
