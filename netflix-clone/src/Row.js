d570ada666e45e52bec9cfaefa5409e07d20087e
palashjain2801 <palashjain2801@gmail.com>
2020-10-11 22:02:13 -0500

    firebase push

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
 import "./Row.css";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
+
 const base_url = "https://image.tmdb.org/t/p/original";
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -14,6 +19,27 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
   // if [] ,  run once when row loads, and dont run again
+  const opts = {
+    height: "390",
+    width: "99%",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const handleClick = (movie) => {
+    // console.table(movie?.title)
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
   return (
     <div className="row">
       {/* Title */}
@@ -22,13 +48,18 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
       </div>
-
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
       {/* container -> poster */}
     </div>
   );
